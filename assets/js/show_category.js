911dca609f0d3571d84e27e86d6f55d20a9f9918
Rakha Dzaky <rakhadzaky9@gmail.com>
2018-11-30 00:35:57 +0700

    adding sorting category laboratorium

--- assets/js/show_category.js
+++ assets/js/show_category.js
@@ -1,4 +1,79 @@
-function toggle () {
-	var x = document.getElementById("icm");
-	console.log(x);
+function toggle (kategori) {
+
+	if (kategori == "icm") {
+		var x = document.querySelectorAll("#icm");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("display_none");
+		}
+
+		var x = document.querySelectorAll("#side");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.add("display_none");
+		}
+
+		var x = document.querySelectorAll("#tele");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.add("display_none");
+		}
+	}
+	else if (kategori == "side") {
+		var x = document.querySelectorAll("#side");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("display_none");
+		}
+
+		var x = document.querySelectorAll("#icm");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.add("display_none");
+		}
+
+		var x = document.querySelectorAll("#tele");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.add("display_none");
+		}
+	}
+	else if (kategori == "tele") {
+		var x = document.querySelectorAll("#tele");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("display_none");
+		}
+
+		var x = document.querySelectorAll("#icm");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.add("display_none");
+		}
+
+		var x = document.querySelectorAll("#side");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.add("display_none");
+		}
+	}
+	else if (kategori == "all") {
+		var x = document.querySelectorAll("#side");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("display_none");
+		}
+
+		var x = document.querySelectorAll("#icm");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("display_none");
+		}
+
+		var x = document.querySelectorAll("#tele");
+
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("display_none");
+		}
+	}
 }
